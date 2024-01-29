c893b3b36944e773eeb642ca08f512f61df7e7e5
SHIRISH-AGARWAL <157955769+SHIRISH-AGARWAL@users.noreply.github.com>
2024-01-29 07:39:50 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict";  // treat all JS code as newer version
+
+// alert( 3 + 3)  // we are using nodejs, not browser
+
+// console.log(3 + 3);   console.log("hitesh")     //bad practice, as we don't have limited lines, code should be readable
+
+// As we initialize value, automatically datatype will generate
+
+let name = "hitesh"                     
+let age = 20                              
+let isloggedin = false                 
+let state;
+
+// number => 2 to the power of 53
+// string => " "
+// bigint
+// boolean => true/false
+// null => standalone value
+// undefined => value is not assigned to a variable
+// symbol => unique
+
+
+// object
+
+console.log(typeof(null))   // object
+console.log(typeof(undefined)) // undefined
